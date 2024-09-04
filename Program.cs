869726845509710ff5ad7204bce8ce1093280cca
Andrey Kerges Nascimento <andrey.nascimento2@fatec.sp.gov.br>
2024-09-04 12:42:41 -0300

    Exercício Finalizado

--- Program.cs
+++ Program.cs
@@ -12,16 +12,16 @@ produtoVend = Convert.ToDouble(Console.ReadLine());
 if (produtoVend <= 250)
 {
     comissao = produtoVend * 1;
-    Console.WriteLine($"Com base na quantidade de produtos vendidos, sua comissão é de: {comissao}");
+    Console.WriteLine($"Com base na quantidade de produtos vendidos {produtoVend}, sua comissão é de: {comissao}");
 
 }
 else if (produtoVend <= 500)
 {
     comissao = produtoVend * 2.50;
-    Console.WriteLine($"Com base na quantidade de produtos vendidos, sua comissão é de: {comissao}");
+    Console.WriteLine($"Com base na quantidade de produtos vendidos {produtoVend}, sua comissão é de: {comissao}");
 }
 else
 {
     comissao = produtoVend * 3;
-    Console.WriteLine($"Com base na quantidade de produtos vendidos, sua comissão é de: {comissao}");
+    Console.WriteLine($"Com base na quantidade de produtos vendidos {produtoVend}, sua comissão é de: {comissao}");
 }
